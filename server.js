3f4b48072b8479a850bf0cd8f5bc2328ba49e81a
AnirbanChattopadhyay-2004 <anirban.chtt@gmail.com>
2024-01-30 22:26:24 +0530

    Commited some changes

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+import 'dotenv/config'; 
+const app=express();
+const port=3000;
+const apikey=process.env.API_KEY;
+// console.log(apikey)
+app.use(express.static("public"))
+app.get("/",(req,res)=>{
+    res.render("index.ejs",{
+        url:"images.png"
+    })
+})
+app.use(bodyParser.urlencoded({extended:true}))
+app.post("/apod",async (req,res)=>{
+    const responseimg=await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apikey}&date=${req.body.date}`)
+    res.render("index.ejs",{
+        url:responseimg.data.url
+    })
+})
+app.listen(port,()=>{
+    console.log("Listening on port "+port)
+})
